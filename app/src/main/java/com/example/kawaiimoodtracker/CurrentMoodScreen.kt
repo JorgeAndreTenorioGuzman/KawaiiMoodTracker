a2f53aa36d6e096b90ab330ccbb9c227fe6ebb01
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-08-01 11:12:56 -0600

    Defined composables

---
+++ app/src/main/java/com/example/kawaiimoodtracker/CurrentMoodScreen.kt
@@ -0,0 +1,9 @@
+package com.example.kawaiimoodtracker
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CurrentMoodScreen(modifier: Modifier = Modifier) {
+
+}
--- app/src/main/java/com/example/kawaiimoodtracker/MainActivity.kt
+++ app/src/main/java/com/example/kawaiimoodtracker/MainActivity.kt
@@ -19,29 +19,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KawaiiMoodTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun App(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KawaiiMoodTrackerTheme {
-        Greeting("Android")
-    }
 }
