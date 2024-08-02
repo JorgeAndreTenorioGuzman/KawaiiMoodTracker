653a69696c723bb15202c2b53154a36ec29bad63
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-08-02 13:49:49 -0600

    created basic ui elements ofCurrentMoodScreen

--- app/src/main/java/com/example/kawaiimoodtracker/CurrentMoodScreen.kt
+++ app/src/main/java/com/example/kawaiimoodtracker/CurrentMoodScreen.kt
@@ -1,17 +1,198 @@
 package com.example.kawaiimoodtracker
 
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.AddCircle
+import androidx.compose.material.icons.sharp.Close
+import androidx.compose.material.icons.sharp.DateRange
+import androidx.compose.material.icons.sharp.Refresh
+import androidx.compose.material.icons.sharp.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.sin
 
 @Composable
 fun CurrentMoodScreen(modifier: Modifier = Modifier) {
 
-    
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(start = 48.dp, end = 48.dp, bottom = 48.dp)
+    ) {
+        Row (modifier =  modifier.fillMaxWidth()){
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Sharp.DateRange,
+                    modifier = modifier
+                        .size(48.dp),
+                    contentDescription = "Mood Calendar Button")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(onClick = { /*TODO*/}) {
+                Icon(
+                    imageVector = Icons.Sharp.AddCircle,
+                    modifier = modifier
+                        .size(48.dp),
+                    contentDescription = "Add mood Button" )
+            }
+        }
+
+        Column (horizontalAlignment = Alignment.CenterHorizontally,){
+            Row (modifier =  modifier.width(230.dp)) {
+                IconButton(
+                    onClick = { /*TODO*/ },
+
+                    ) {
+                    Icon(
+                        imageVector = Icons.Sharp.Refresh,
+                        contentDescription = "Generate quote"
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(
+                    onClick = { /*TODO*/ },
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Sharp.Close,
+                        contentDescription = "Generate quote"
+                    )
+                }
+            }
+            Text(
+                text = "Generate quote",
+                modifier = modifier
+                    .width(271.dp),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = modifier.height(16.dp))
+            Box(
+                modifier = modifier
+                    .size(224.dp)
+                    .clip(MaterialTheme.shapes.medium)
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    modifier = modifier
+                        .fillMaxSize()
+                        .clip(MaterialTheme.shapes.medium),
+
+                    contentDescription = "Expression Image")
+            }
+            Spacer(modifier = modifier.height(16.dp))
+            Text(
+                text = "Awesome",
+                fontSize = 24.sp,
+                modifier = modifier
+                    .width(187.dp)
+                    .height(39.dp),
+                textAlign = TextAlign.Center,
+            )
+
+            Text(
+                text = "July 29",
+                fontSize = 20.sp,
+                modifier = modifier
+                    .width(103.dp)
+                    .height(23.dp),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "9:00 pm",
+                fontSize = 20.sp,
+                modifier = modifier
+                    .width(103.dp)
+                    .height(23.dp),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = modifier.height(16.dp))
+
+            RecordReason()
+
+        }
+    }
 
 }
 
+@Composable
+fun RecordReason(modifier: Modifier = Modifier) {
+
+    Row (
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .width(212.dp)
+                .height(41.dp),
+            shape = MaterialTheme.shapes.medium,
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+            )
+
+        )
+        Spacer(modifier = modifier.width(4.dp))
+        Button(
+            onClick = { /*TODO*/},
+            modifier = Modifier
+                .size(41.dp)
+                .padding(0.dp),
+            shape = MaterialTheme.shapes.medium,
+
+
+        ) {
+            Icon(
+                imageVector = Icons.Sharp.Send,
+                contentDescription = "send",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+        }
+    }
+}
+
 @Preview
+@Composable
+private fun RecordReasonPreview() {
+    RecordReason()
+}
+
+@Preview(showBackground = true)
 @Composable
 private fun CurrentMoodScreenPreview() {
     CurrentMoodScreen()
