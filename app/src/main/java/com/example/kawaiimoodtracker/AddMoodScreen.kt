9a79bd2085eb171a865fdc588a8b18955f2f1089
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-08-02 10:42:58 -0600

    completed adding the basic ui elements to the AddMoodScreen

--- app/src/main/java/com/example/kawaiimoodtracker/AddMoodScreen.kt
+++ app/src/main/java/com/example/kawaiimoodtracker/AddMoodScreen.kt
@@ -3,9 +3,11 @@ package com.example.kawaiimoodtracker
 import android.text.Layout
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -13,14 +15,23 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material.icons.sharp.AddCircle
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -32,25 +43,40 @@ import com.example.kawaiimoodtracker.ui.theme.KawaiiMoodTrackerTheme
 
 @Composable
 fun AddMoodScreen(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(42.dp)
     ) {
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = modifier.align(Alignment.End)
+        ) {
+            Icon(
+                imageVector = Icons.Sharp.Close,
+                contentDescription = stringResource(id = R.string.close_addmoodscreen),
+                modifier = modifier.size(48.dp)
+            )
+        }
         Text(
             text = stringResource(id = R.string.how_are_you),
             textAlign = TextAlign.Center,
             fontSize = 32.sp,
             modifier = modifier
                 .width(272.dp)
-                .height(96.dp)
+                .height(96.dp),
+            lineHeight = 39.sp
         )
+        
         Button(
             onClick = { /*TODO*/ },
             shape = MaterialTheme.shapes.medium,
             modifier = modifier
                 .width(224.dp)
                 .height(224.dp)
-                //.padding(48.dp)
+
 
         ) {
            Icon(
@@ -60,11 +86,25 @@ fun AddMoodScreen(modifier: Modifier = Modifier) {
                    .height(61.dp),
                contentDescription = stringResource(id = R.string.add_expression))
         }
+        Spacer(modifier = modifier.height(16.dp))
+        TextField(
+            value = text,
+            onValueChange = { text = it},
+            label = { Text(text = stringResource(id = R.string.name_feeling))},
+            shape = MaterialTheme.shapes.medium,
+            modifier = modifier
+                .width(257.dp)
+                .height(48.dp)
+
+
+
+        )
+        Spacer(modifier = modifier.height(16.dp))
         Button(
             onClick = { /*TODO*/ },
             enabled = false,
             modifier = Modifier
-                .padding(24.dp)
+                // .padding(24.dp)
                 .height(46.dp)
                 .width(224.dp)
 
