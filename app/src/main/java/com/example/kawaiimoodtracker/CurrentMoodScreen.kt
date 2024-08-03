2f03d4d277de3bd1263975e2791d6cbde2586738
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-08-03 10:36:13 -0600

    changed name of DataTimeMood Composable

--- app/src/main/java/com/example/kawaiimoodtracker/CurrentMoodScreen.kt
+++ app/src/main/java/com/example/kawaiimoodtracker/CurrentMoodScreen.kt
@@ -1,7 +1,6 @@
 package com.example.kawaiimoodtracker
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,9 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.AddCircle
 import androidx.compose.material.icons.sharp.Close
@@ -23,7 +19,6 @@ import androidx.compose.material.icons.sharp.DateRange
 import androidx.compose.material.icons.sharp.Edit
 import androidx.compose.material.icons.sharp.Refresh
 import androidx.compose.material.icons.sharp.Send
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -35,7 +30,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -67,7 +61,7 @@ fun CurrentMoodScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = modifier.height(16.dp))
 
        //date and time
-        DateandTimeofMood()
+        DateTimeMood()
         
         Spacer(modifier = modifier.height(16.dp))
 
@@ -80,7 +74,7 @@ fun CurrentMoodScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DateandTimeofMood(modifier: Modifier = Modifier) {
+fun DateTimeMood(modifier: Modifier = Modifier) {
     Text(
         text = "July 29",
         fontSize = 20.sp,
